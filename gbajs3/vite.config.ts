3b3c223247101478bd4d29c58a0564e90dda4959
Nicholas VanCise <40526638+thenick775@users.noreply.github.com>
2025-01-19 10:21:46 -0800

    chore: upgrade deps (#245)
    * chore: update deps
    
    * chore: upgrade deps
    
    * fix: snapshots

--- gbajs3/vite.config.ts
+++ gbajs3/vite.config.ts
@@ -1,4 +1,4 @@
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import { coverageConfigDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
