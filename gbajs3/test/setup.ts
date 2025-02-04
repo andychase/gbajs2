11e6d1feff598b56ed93d713cc0b7acd6ddd64d3
Nicholas VanCise <40526638+thenick775@users.noreply.github.com>
2025-02-03 17:24:51 -0800

    chore: upgrade node (#260)

--- gbajs3/test/setup.ts
+++ gbajs3/test/setup.ts
@@ -4,6 +4,9 @@ import { afterAll, afterEach, beforeAll, vi } from 'vitest';
 import { gbaServerLocationPlaceholder } from './mocks/handlers.ts';
 import { server } from './mocks/server.ts';
 
+// see: https://github.com/mswjs/msw/issues/2166#issuecomment-2363457424
+import 'blob-polyfill';
+
 // MSW setup
 vi.stubEnv('VITE_GBA_SERVER_LOCATION', gbaServerLocationPlaceholder);
 
